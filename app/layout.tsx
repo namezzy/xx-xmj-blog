f919607ebfdb08fa59e9db00cc209a1fa8eba955
Levi <xmj@xx-xmj.com>
2025-10-23 17:08:38 +0800

    Update page title to 'Levi's Home Page'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Levi's Website",
+  title: "Levi's Home Page",
   description: "享受当下，无限进步",
 };
 
